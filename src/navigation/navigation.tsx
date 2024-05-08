2c761570756cb1782a2d52ec9bbcf43da4aa7601
keertisomu <maverick.somu@gmail.com>
2024-05-08 19:59:22 +1000

    initial commit

---
+++ src/navigation/navigation.tsx
@@ -0,0 +1,11 @@
+export const Navigation = () => {
+
+    return (
+        <div>
+        <a href="#home">Home</a>
+        <a href="#projects">Projects</a>
+        <a href="#skills">Skills</a>
+    </div>
+    )
+
+}
